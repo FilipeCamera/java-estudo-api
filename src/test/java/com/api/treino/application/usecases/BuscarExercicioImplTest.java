e0157eca2381a8e3e62828d38b3b99b6754f1ddd
Filipe Camera <filipecameracastro@gmail.com>
2024-10-13 22:39:35 -0300

    fazendo ajuste

--- src/test/java/com/api/treino/application/usecases/BuscarExercicioImplTest.java
+++ src/test/java/com/api/treino/application/usecases/BuscarExercicioImplTest.java
@@ -1,66 +1,67 @@
 package com.api.treino.application.usecases;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.List;
-import org.junit.jupiter.api.Test;
+
 import com.api.treino.application.gateway.IBuscarExercicioGatewayTest;
 import com.api.treino.core.domain.personal.Exercicio;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BuscarExercicioImplTest {
 
-  @Test
-  public void buscarExercicios() {
-    IBuscarExercicioGatewayTest buscarExercicioGatewayTest = new IBuscarExercicioGatewayTest();
-    BuscarExercicioImpl buscarExercicioImpl = new BuscarExercicioImpl(buscarExercicioGatewayTest);
+    private IBuscarExercicioGatewayTest buscarExercicioGatewayTest;
+    private BuscarExercicioImpl buscarExercicioImpl;
 
-    try {
-      List<Exercicio> exercicios = buscarExercicioImpl.buscar(buscarExercicioGatewayTest.personal);
-
-      assertEquals(3, exercicios.size());
-    } catch (Exception e) {
-      //
+    @BeforeEach
+    public void carregarDados() {
+        buscarExercicioGatewayTest = new IBuscarExercicioGatewayTest();
+        buscarExercicioImpl = new BuscarExercicioImpl(buscarExercicioGatewayTest);
     }
-  }
 
-  @Test
-  public void buscarExercicioPorId() {
-    IBuscarExercicioGatewayTest buscarExercicioGatewayTest = new IBuscarExercicioGatewayTest();
-    BuscarExercicioImpl buscarExercicioImpl = new BuscarExercicioImpl(buscarExercicioGatewayTest);
+    @Test
+    public void buscarExercicios() {
+        try {
+            List<Exercicio> exercicios = buscarExercicioImpl.buscar(buscarExercicioGatewayTest.personal);
 
-    try {
-      Exercicio exercicio = buscarExercicioImpl.buscar(buscarExercicioGatewayTest.personal,
-          buscarExercicioGatewayTest.id);
-
-      assertEquals("test", exercicio.getNome());
-      assertEquals(buscarExercicioGatewayTest.id, exercicio.getId());
-    } catch (Exception e) {
-      //
+            assertEquals(3, exercicios.size());
+        } catch (Exception e) {
+            //
+        }
     }
-  }
 
-  @Test
-  public void erroAoBuscarExercicio() {
-    IBuscarExercicioGatewayTest buscarExercicioGatewayTest = new IBuscarExercicioGatewayTest();
-    BuscarExercicioImpl buscarExercicioImpl = new BuscarExercicioImpl(buscarExercicioGatewayTest);
+    @Test
+    public void buscarExercicioPorId() {
+        try {
+            Exercicio exercicio = buscarExercicioImpl.buscar(
+                buscarExercicioGatewayTest.personal,
+                buscarExercicioGatewayTest.id
+            );
 
-    try {
-      buscarExercicioGatewayTest.limparBanco();
-      buscarExercicioImpl.buscar(null);
-    } catch (Exception e) {
-      assertEquals("Nenhum exercicio encontrado", e.getMessage());
+            assertEquals("test", exercicio.getNome());
+            assertEquals(buscarExercicioGatewayTest.id, exercicio.getId());
+        } catch (Exception e) {
+            //
+        }
     }
-  }
 
-  @Test
-  public void erroAoBuscarExercicioPorId() {
-    IBuscarExercicioGatewayTest buscarExercicioGatewayTest = new IBuscarExercicioGatewayTest();
-    BuscarExercicioImpl buscarExercicioImpl = new BuscarExercicioImpl(buscarExercicioGatewayTest);
-
-    try {
-      buscarExercicioGatewayTest.limparBanco();
-      buscarExercicioImpl.buscar(buscarExercicioGatewayTest.personal, null);
-    } catch (Exception e) {
-      assertEquals("Nao foi encontrado o exercicio", e.getMessage());
+    @Test
+    public void erroAoBuscarExercicio() {
+        try {
+            buscarExercicioGatewayTest.limparBanco();
+            buscarExercicioImpl.buscar(null);
+        } catch (Exception e) {
+            assertEquals("Nenhum exercicio encontrado", e.getMessage());
+        }
     }
-  }
+
+    @Test
+    public void erroAoBuscarExercicioPorId() {
+        try {
+            buscarExercicioGatewayTest.limparBanco();
+            buscarExercicioImpl.buscar(buscarExercicioGatewayTest.personal, null);
+        } catch (Exception e) {
+            assertEquals("Nao foi encontrado o exercicio", e.getMessage());
+        }
+    }
 }
--- src/test/java/com/api/treino/application/usecases/CriarAlunoImplTest.java
+++ src/test/java/com/api/treino/application/usecases/CriarAlunoImplTest.java
@@ -1,43 +1,47 @@
 package com.api.treino.application.usecases;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+
 import com.api.treino.application.gateway.ICriarAlunoGatewayTest;
 import com.api.treino.core.domain.UsuarioData;
 import com.api.treino.core.domain.aluno.Aluno;
 import com.api.treino.core.domain.aluno.AlunoData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CriarAlunoImplTest {
 
-  @Test
-  public void criarAluno() {
-    ICriarAlunoGatewayTest criarAlunoGatewayTest = new ICriarAlunoGatewayTest();
-    CriarAlunoImpl criarAlunoImpl = new CriarAlunoImpl(criarAlunoGatewayTest);
+    CriarAlunoImpl criarAlunoImpl;
 
-    UsuarioData usuario = new UsuarioData("teste", "aluno", "teste@email.com", "teste");
-    AlunoData info = new AlunoData(22, 1.65, 58.6);
-
-    try {
-      Aluno aluno = criarAlunoImpl.exec(usuario, info);
-
-      assertEquals("teste", aluno.usuario().getNome());
-      assertEquals("teste@email.com", aluno.usuario().getEmail());
-      assertEquals("aluno", aluno.usuario().getSobrenome());
-      assertEquals("teste", aluno.usuario().getSenha());
-    } catch (Exception e) {
-
+    @BeforeEach
+    public void carregarDados() {
+        ICriarAlunoGatewayTest criarAlunoGatewayTest = new ICriarAlunoGatewayTest();
+        this.criarAlunoImpl = new CriarAlunoImpl(criarAlunoGatewayTest);
     }
-  }
 
-  @Test
-  public void gerarErroAoCriarAluno() {
-    ICriarAlunoGatewayTest criarAlunoGatewayTest = new ICriarAlunoGatewayTest();
-    CriarAlunoImpl criarAlunoImpl = new CriarAlunoImpl(criarAlunoGatewayTest);
+    @Test
+    public void criarAluno() {
+        UsuarioData usuario = new UsuarioData("teste", "aluno", "teste@email.com", "teste");
+        AlunoData info = new AlunoData(22, 1.65, 58.6);
 
-    try {
-      criarAlunoImpl.exec(null, null);
-    } catch (Exception e) {
-      assertEquals("Nao foi possivel criar o usuario", e.getMessage());
+        try {
+            Aluno aluno = criarAlunoImpl.exec(usuario, info);
+
+            assertEquals("teste", aluno.usuario().getNome());
+            assertEquals("teste@email.com", aluno.usuario().getEmail());
+            assertEquals("aluno", aluno.usuario().getSobrenome());
+            assertEquals("teste", aluno.usuario().getSenha());
+        } catch (Exception e) {
+            // nao pega o erro
+        }
     }
-  }
+
+    @Test
+    public void gerarErroAoCriarAluno() {
+        try {
+            criarAlunoImpl.exec(null, null);
+        } catch (Exception e) {
+            assertEquals("Nao foi possivel criar o usuario", e.getMessage());
+        }
+    }
 }
